836d831c8e5f68af51d4e76dcfa0b51d1eaa9d85
marwanElmohr <marwankhalil2002@gmail.com>
2023-10-11 01:10:08 +0300

    Sidebar routes updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,8 @@
-import AddPatient from "./Pages/AddPatient";
-import AddPharmacist from "./Pages/AddPharmacist";
-import ViewMedPatientCopy from "./Pages/ViewMedPatient copy";
-import ViewMedPharm from "./Pages/ViewMedPharm";
-import ViewMedPharmCopy from "./Pages/ViewMedPharm copy";
-
+import AddAdmin from "./Pages/AddAdmin";
 
 function App() {
   return (
-    <AddPharmacist />
+    <AddAdmin />
   );
 }
 
--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Admin from './Pages/Admin'
+import AddAdmin from './Pages/AddAdmin';
+import ViewPharmInfoAdmin from './Pages/ViewPharmInfoAdmin';
+import ViewPatientInfoAdmin from './Pages/ViewPatientInfoAdmin';
+import ViewRequestsAdmin from './Pages/ViewRequestsAdmin';
 import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Admin" element={<Admin />} />
+        <Route path="/AddAdmin" element={<AddAdmin />} />
+        <Route path="/ViewPharmInfoAdmin" element={<ViewPharmInfoAdmin />} />
+        <Route path="/ViewPatientInfoAdmin" element={<ViewPatientInfoAdmin />} />
+        <Route path="/ViewRequestsAdmin" element={<ViewRequestsAdmin />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
