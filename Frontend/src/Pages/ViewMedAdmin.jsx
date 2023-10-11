8a894ece2c9087363e79f7b7b688638de0229b5f
ZiadYasser411 <ziadyasser4111@gmail.com>
2023-10-12 00:00:33 +0300

    ViewMed for admin, patient

--- Frontend/src/Pages/ViewMedAdmin.jsx
+++ Frontend/src/Pages/ViewMedAdmin.jsx
@@ -1,16 +1,17 @@
 import MedTableAll from "../Components/MedTableAll";
 import Sidebar from '../Components/Sidebar';
+import MedTableAllCopy from "../Components/MedTableAll copy";
 
 function ViewMedAdmin() {
   return (
     <div>
-        <div id="outer-container">
-            <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-            <div id="page-wrap">
-              <MedTableAll />
-            </div>
+      <div id="outer-container">
+        <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+        <div id="page-wrap">
+          <MedTableAllCopy />
         </div>
       </div>
+    </div>
   );
 }
 
